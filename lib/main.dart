419ee6790bfd5e4cbdfe5a83e3268875aadc22fa
muhammed ghazi <muhamedghazi79@gmail.com>
2025-05-01 17:03:11 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WaterMtr());
+}
+
+class WaterMtr extends StatelessWidget {
+  const WaterMtr({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
