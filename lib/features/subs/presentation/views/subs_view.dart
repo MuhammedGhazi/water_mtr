9bae6715f50d8cf5d903c03740b89747d64958cc
muhammed ghazi <muhamedghazi79@gmail.com>
2025-05-02 22:25:49 +0300

    add modelbottom sheet

--- lib/features/subs/presentation/views/subs_view.dart
+++ lib/features/subs/presentation/views/subs_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:water_mtr/features/subs/presentation/widgets/subs_view_body.dart';
+import 'package:water_mtr/features/template/presentation/views/widgets/add_note_bottom_sheet.dart';
 
 class SubsView extends StatelessWidget {
   const SubsView({super.key});
@@ -8,7 +9,12 @@ class SubsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) => AddNoteBottomSheet(),
+          );
+        },
         child: Icon(Icons.add),
       ),
       body: SubsViewBody(),
---
+++ lib/features/subs/presentation/widgets/add_sub_bottom_sheet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddSubBottomSheet extends StatelessWidget {
+  const AddSubBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
